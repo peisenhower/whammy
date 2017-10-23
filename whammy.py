5464d758ef032b71acac4f4b26590122434d7ccc
peter eisenhower <peter.eisenhower@gmail.com>
2017-10-22 21:33:52 -0400

    adding stub whammy code

---
+++ whammy.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+# import RPi.GPIO as GPIO
+import pygame
+import time
+
+
+# BUTTON_PIN = 17 # Broadcom pin 17 (P1 pin 11)
+
+# def button_setup():
+#     GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# def is_button_pressed():
+#     # button is low when pressed
+#     return !GPIO.input(BUTTON_PIN)
+
+
+def game():
+    pass
+
+def idle():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
