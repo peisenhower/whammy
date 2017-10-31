f9fed8a895d23065c6dca2136d701f93a6691fa1
Andrew Karetas <ajkaretas@gmail.com>
2017-10-31 21:49:07 +0000

    Finalize gameplay

--- led.py
+++ led.py
@@ -1,30 +1,67 @@
-
+#!/usr/bin/python3
 import threading
-#import Adafruit_PCA9685
+import Adafruit_PCA9685
 import time
 
-FADE_INTERVAL = 0.2
-FADE_INCREMENT = 10
+FADE_INTERVAL = 0.1
+FADE_INCREMENT = 200
 LED_VALUES = [100] * 10
 
+# Configure min and max servo pulse lengths
+servo_min = 50  # Min pulse length out of 4096
+servo_max = 400  # Max pulse length out of 4096
+
 # Initialise the PCA9685 using the default address (0x40).
-#pwm = Adafruit_PCA9685.PCA9685()
+pwm = Adafruit_PCA9685.PCA9685()
+pwm.set_pwm_freq(60)
+pwm.set_pwm(10, 0, 2047)
 
 def fade_callback(led):
-    if LED_VALUES[led] <= 0:
+    if LED_VALUES[led] >= 4095:
         return
 
-    LED_VALUES[led] -= FADE_INCREMENT
+    LED_VALUES[led] += FADE_INCREMENT
+    if LED_VALUES[led] > 4094:
+        LED_VALUES[led] = 4095
 
     #print(f'Led Update, Led:{led}, Value:{LED_VALUES[led]}')
-    # todo i2c call here
+    pwm.set_pwm(led, 0, LED_VALUES[led])
     threading.Timer(FADE_INTERVAL, fade_callback, [led]).start()
 
 
 def fade(led):
     threading.Timer(FADE_INTERVAL, fade_callback, [led]).start()
 
-def blink(led):
-    #pwm.setPWM(led, 0, 4095)
-    #time.sleep(1)
-    #pwm.setPwm(led, 4095, 0)
+
+def off(led):
+    pwm.set_pwm(led, 0, 4095)
+    LED_VALUES[led] = 4095
+
+def on(led):
+    pwm.set_pwm(led, 0, 0)
+    LED_VALUES[led] = 0
+
+def blink(old_led, led):
+    pwm.set_pwm(led, 0, 0)
+    pwm.set_pwm(old_led, 0, 4095)
+
+def whammy():
+    pwm.set_pwm(9, 0, servo_max)
+    time.sleep(.4)
+    pwm.set_pwm(9, 0, servo_min)
+
+
+
+if __name__ == "__main__":
+    on(0)
+    on(1)
+    on(2)
+    on(3)
+
+    time.sleep(1)
+
+    off(0)
+    off(1)
+    off(2)
+    off(3)
+
--- whammy.py
+++ whammy.py
@@ -38,15 +38,19 @@ def sound(name):
     pygame.mixer.music.fadeout(1)
     pygame.mixer.music.load(SOUNDS_TABLE[name])
     pygame.mixer.music.play()
+
+
 def music():
     pygame.mixer.music.fadeout(1)
     pygame.mixer.music.load(MUSIC_LIST[random.randint(0,5)])
     pygame.mixer.music.play()
 
+
 def game():
     print("game on")
     global game_state
     game_state = "game"
+    reset_lights()
 
 def idle():
     global game_state
@@ -54,37 +58,59 @@ def idle():
     print("going idle")
     music()
 
+def reset_lights():
+    led.off(0)
+    led.off(1)
+    led.off(2)
+    led.off(3)
+    led.off(4)
+    led.off(5)
+    led.off(6)
+    led.off(7)
+    led.off(8)
+
 def non_repeating_random(num):
-    val = random.randint(1,9)
+    val = random.randint(0,8)
     while val == num:
-        val = random.randint(1,9)
+        val = random.randint(0,8)
     return val
+
 
 def end_game(pin):
     print("pressed from game")
     if check_win(pin) == 1:
         #normal win
+        #led.candy()
+        sound("happy-halloween")
+        time.sleep(8)
         print("normal: "+str(pin))
     elif check_win(pin) == 2:
         #double win
+        #led.candy()
+        sound("happy-halloween")
+        time.sleep(8)
         print("double: "+str(pin))
     elif check_win(pin) == 3:
         #whammy
+        sound("laugh")
+        led.whammy()
         print("whammy: "+str(pin))
-    time.sleep(1)
+        time.sleep(5)
     idle()
 
 def check_win(pin):
-    if pin == 1 or pin == 3 or pin == 4 or pin == 6 or pin == 8:
+    if pin == 0 or pin == 2 or pin == 3 or pin == 5 or pin == 7:
         return 1
-    if pin == 2 or pin == 7:
+    if pin == 1 or pin == 6:
         return 2
-    if pin == 5 or pin == 9:
+    if pin == 4 or pin == 8:
         return 3
 
 def main():
+    reset_lights()
     count = 0 #slows down the loop
     pin = 0 #pin number of LED to light
+    old_pin = 0
     idle()
     while True:
         count+=1
@@ -104,9 +130,10 @@ def main():
         #Game blinks at higher rate
         if count == 25 or count == 50 or count == 75:
             if game_state == "game":
+                old_pin = pin
                 pin = non_repeating_random(pin)
-                print("blink "+str(pin))
-                led.blink(pin)
+                #print("blink "+str(pin))
+                led.blink(old_pin,pin)
 
         if count > 100:
             count = 0
@@ -120,14 +147,17 @@ def main():
           
             #depending on state blink lights
             if game_state == "game":
+                old_pin = pin
                 pin = non_repeating_random(pin)
-                print("blink "+ str(pin))
-                led.blink(pin)
+                #print("blink "+ str(pin))
+                led.blink(old_pin,pin)
             elif game_state == "idle":
                 pin = non_repeating_random(pin)
-                print("fade "+str(pin))
+                #print("fade "+str(pin))
+                led.on(pin)
                 led.fade(pin)
         time.sleep(.01) #slow loop down a bit
+
 
 if __name__ == "__main__":
     pygame.mixer.init()
